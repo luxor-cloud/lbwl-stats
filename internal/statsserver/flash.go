346ff35a0bf61578b05d1f33a8e28dd275b95855
Yannic Rieger <riegeryannic@gmail.com>
2020-01-09 11:19:59 +0100

    Error refactoring

--- internal/statsserver/flash.go
+++ internal/statsserver/flash.go
@@ -7,27 +7,23 @@ import (
 	"github.com/twitchtv/twirp"
 )
 
+var (
+	InvalidIDError = twirp.InvalidArgumentError("playerId", "cannot be empty")
+	InvalidMapError = twirp.InvalidArgumentError("maps", "cannot be empty")
+)
+
 func (s *Server) GetFlashMapStats(ctx context.Context, mapReq *service.GetFlashMapStatsRequest) (*service.GetFlashStatsResponse, error) {
 	if mapReq.GetPlayerId() == "" {
-		return nil, twirp.InvalidArgumentError("playerId", "cannot be empty")
+		return nil, InvalidIDError
 	}
 
 	if len(mapReq.GetMaps()) == 0 {
-		return nil, twirp.InvalidArgumentError("maps", "cannot be empty")
+		return nil, InvalidMapError
 	}
 
-	// We could compute all of them in their own goroutine which would make this part faster,
-	// but we just leave it like this for now.
-	maps := make([]*model.FlashMapStatistic, 0)
-	for _, m := range mapReq.GetMaps() {
-		stats, err := s.flashDAO.GetMapStatistic(mapReq.GetPlayerId(), m)
-		if err != nil {
-			return nil, err
-		}
-
-		if stats != nil {
-			maps = append(maps, stats)
-		}
+	maps, err := s.getMapStats(mapReq.GetPlayerId(), mapReq.GetMaps())
+	if err != nil {
+		return nil, err
 	}
 
 	return &service.GetFlashStatsResponse{
@@ -39,7 +35,7 @@ func (s *Server) GetFlashMapStats(ctx context.Context, mapReq *service.GetFlashM
 
 func (s *Server) GetFlashGameStats(ctx context.Context, gameReq *service.GetFlashGameStatsRequest) (*service.GetFlashStatsResponse, error) {
 	if gameReq.GetPlayerId() == "" {
-		return nil, twirp.InvalidArgumentError("playerId", "cannot be empty.")
+		return nil, InvalidIDError
 	}
 
 	stats, err := s.flashDAO.GetGameStatistic(gameReq.GetPlayerId())
@@ -55,5 +51,45 @@ func (s *Server) GetFlashGameStats(ctx context.Context, gameReq *service.GetFlas
 }
 
 func (s *Server) GetFlashStats(ctx context.Context, compReq *service.GetFlashStatsCompoundRequest) (*service.GetFlashStatsResponse, error) {
-	return nil, nil
+	if compReq.GetPlayerId() == "" {
+		return nil, InvalidIDError
+	}
+
+	if len(compReq.GetMaps()) == 0 {
+		return nil, InvalidMapError
+	}
+
+	gameStats, err := s.flashDAO.GetGameStatistic(compReq.GetPlayerId())
+	if err != nil {
+		return nil, err
+	}
+
+	mapStats, err := s.getMapStats(compReq.GetPlayerId(), compReq.GetMaps())
+	if err != nil {
+		return nil, err
+	}
+
+	return &service.GetFlashStatsResponse{
+		Stats: &model.FlashStatisticCompound{
+			GameSummary: gameStats,
+			MapSummary:  mapStats,
+		},
+	}, nil
+}
+
+func (s *Server) getMapStats(id string, mapNames []string) ([]*model.FlashMapStatistic, error) {
+	// We could compute all of them in their own goroutine which would make this part faster,
+	// but we just leave it like this for now.
+	maps := make([]*model.FlashMapStatistic, 0)
+	for _, m := range mapNames {
+		stats, err := s.flashDAO.GetMapStatistic(id, m)
+		if err != nil {
+			return nil, err
+		}
+
+		if stats != nil {
+			maps = append(maps, stats)
+		}
+	}
+	return maps, nil
 }
--- internal/statsserver/flash_test.go
+++ internal/statsserver/flash_test.go
@@ -141,7 +141,7 @@ func TestServer_GetFlashMapStats_Empty_Map_Should_Throw_Error(t *testing.T) {
 
 	_, err := s.GetFlashMapStats(nil, req)
 
-	assert.EqualError(t, err, "twirp error invalid_argument: maps cannot be empty")
+	assert.EqualError(t, err, InvalidMapError.Error())
 }
 
 func TestServer_GetFlashMapStats_Empty_ID_Should_Throw_Error(t *testing.T) {
@@ -152,7 +152,7 @@ func TestServer_GetFlashMapStats_Empty_ID_Should_Throw_Error(t *testing.T) {
 
 	_, err := s.GetFlashMapStats(nil, req)
 
-	assert.EqualError(t, err, "twirp error invalid_argument: playerId cannot be empty")
+	assert.EqualError(t, err, InvalidIDError.Error())
 }
 
 
@@ -187,7 +187,7 @@ func TestServer_GetFlashGameStats_Empty_ID_Should_Throw_Error(t *testing.T) {
 	req := &service.GetFlashGameStatsRequest{}
 	_, err := s.GetFlashGameStats(nil, req)
 
-	assert.EqualError(t, err, "twirp error invalid_argument: playerId cannot be empty")
+	assert.EqualError(t, err, InvalidIDError.Error())
 }
 
 //
@@ -195,5 +195,62 @@ func TestServer_GetFlashGameStats_Empty_ID_Should_Throw_Error(t *testing.T) {
 //
 
 func TestServer_GetFlashStats_Valid_Response(t *testing.T) {
+	s := getMockServer()
+	req := &service.GetFlashStatsCompoundRequest{
+		PlayerId:             "92de217b-8b2b-403b-86a5-fe26fa3a9b5f",
+		Maps:                 []string{ "Map1" },
+	}
 
+	resp, err := s.GetFlashStats(nil, req)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+
+	game := resp.GetStats().GetGameSummary()
+	assert.NotNil(t, game)
+	assert.Equal(t, uint32(1200), game.GetWins())
+	assert.Equal(t, uint32(1337), game.GetDeaths())
+	assert.Equal(t, uint32(200), game.GetGamesPlayed())
+	assert.Equal(t, uint32(200), game.GetInstantDeaths())
+	assert.Equal(t, uint32(500), game.GetCheckpoints())
+
+	maps := resp.GetStats().GetMapSummary()
+	assert.Len(t, maps, 1)
+
+	assert.Equal(t, "Map1", maps[0].GetName())
+	assert.Equal(t, "2018-03-05 16:00:23", maps[0].GetAccomplishedAt())
+	assert.Equal(t, uint64(200), maps[0].GetRecordTime())
+
+	checkpoints := maps[0].GetCheckpoints()
+	assert.Len(t, checkpoints, 2)
+
+	assert.Equal(t, int32(1), checkpoints[0].GetCheckpoint())
+	assert.Equal(t, uint64(1), checkpoints[0].GetRecordTime())
+	assert.Equal(t, "2011-03-05 16:00:23", checkpoints[0].GetAccomplishedAt())
+
+	assert.Equal(t, int32(2), checkpoints[1].GetCheckpoint())
+	assert.Equal(t, uint64(2), checkpoints[1].GetRecordTime())
+	assert.Equal(t, "2012-03-05 16:00:23", checkpoints[1].GetAccomplishedAt())
+}
+
+func TestServer_GetFlashStats_Empty_ID_Should_Throw_Error(t *testing.T) {
+	s := getMockServer()
+	req := &service.GetFlashStatsCompoundRequest{
+		PlayerId:             "",
+		Maps:                 []string{ "Map1" },
+	}
+
+	_, err := s.GetFlashStats(nil, req)
+	assert.EqualError(t, err, InvalidIDError.Error())
+}
+
+func TestServer_GetFlashStats_Empty_Map_Should_Throw_Error(t *testing.T) {
+	s := getMockServer()
+	req := &service.GetFlashStatsCompoundRequest{
+		PlayerId:             "92de217b-8b2b-403b-86a5-fe26fa3a9b5f",
+		Maps:                 []string{},
+	}
+
+	_, err := s.GetFlashStats(nil, req)
+	assert.EqualError(t, err, InvalidMapError.Error())
 }
